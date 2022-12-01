7662c5bc7435f9185349d3fbc9f92485a2064cf8
Faizan Ahmed <53269044+faizywaizy@users.noreply.github.com>
2022-12-01 10:55:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -40,6 +40,52 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+
+
+          <div>
+            <h1> CPS530 - Lab10 Problem 3</h1>
+
+            <h2>
+
+            I used Reactjs to build this website, deploying using an online hosting service Netlify
+            I installed this framework like this:
+            </h2>
+            <ol>
+              <li>Download and Installed NodeJs following 
+                <a href="https://nodejs.org/en/download/"> this link </a>
+              </li>
+              <li> Changed directories into where my project filestructure should be.
+              </li>
+              <li>
+                executed the following command in my terminal:
+                <code>
+                npx create-react-app my-app
+                </code>
+              </li>
+              <li>
+                Edit the App.js file to reflect the lab requirements
+              </li>
+              <li>
+                install netlify-cli using the following command:
+                <code>
+                  npm install netlify-cli -g
+                </code>
+              </li>
+              <li>
+              Once I was ready to host this live, I executed:
+                <code>
+                netlify deploy
+                </code>
+              </li>
+
+            </ol>
+            <h1>
+              I encountered no major issues running this project.
+              Honestly, for a basic website this process is pretty brainless.
+              Frontend development is in this amazing place where it's so quick to get started.
+              Most of the issues that I encounter in my dev work is connecting backend and front end.
+            </h1>
+          </div>
           <LambdaDemo />
         </header>
       </div>
